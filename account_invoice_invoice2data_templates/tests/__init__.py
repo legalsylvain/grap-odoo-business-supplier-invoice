57464648e8066c59cf1ba9f7eec833f5d823347d
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-02-21 13:00:32 +0100

    [ADD] Croc JBG SAS

--- account_invoice_invoice2data_templates/tests/__init__.py
+++ account_invoice_invoice2data_templates/tests/__init__.py
@@ -3,6 +3,7 @@ from . import test
 from . import test_agrosourcing
 from . import test_comptoir_des_lys
 from . import test_coop_de_yenne
+from . import test_croc_jbg_sas
 from . import test_ekibio
 from . import test_gravier
 from . import test_markal
---
+++ account_invoice_invoice2data_templates/tests/test_croc_jbg_sas.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2023 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from datetime import datetime
+
+from .test import TestModule
+
+
+class TestCrocJBGSAS(TestModule):
+    def test_croc_jbg_sas(self):
+        self._test_supplier_template(
+            "croc-jbg-sas__2023-01-26__FA4549.pdf",
+            line_qty=5,
+            expected_values={
+                "issuer": "Croc JBG SAS",
+                "date": datetime(day=26, month=1, year=2023),
+                "invoice_number": "FA4549",
+                "amount_untaxed": 772.75,
+                "amount": 815.25,
+            },
+            expected_lines=[
+                {
+                    "product_code": "0937",
+                    "product_name": "Graines de Courge nature 10 Kg",
+                    "quantity": 10.00,
+                    "price_unit": 13.20,
+                    "discount": 21.59,
+                    "price_subtotal": 103.50,
+                }
+            ],
+        )
