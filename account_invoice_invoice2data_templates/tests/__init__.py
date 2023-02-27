ab1d6060cd705e497556fdfbca4af239b429a46b
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-02-27 17:37:21 +0100

    [ADD] Scop Epice template

--- account_invoice_invoice2data_templates/tests/__init__.py
+++ account_invoice_invoice2data_templates/tests/__init__.py
@@ -4,14 +4,13 @@ from . import test_agrosourcing
 from . import test_comptoir_des_lys
 from . import test_coop_de_yenne
 from . import test_croc_jbg_sas
-
 from . import test_ekibio
 from . import test_gonuts
 from . import test_gravier
-
 from . import test_markal
 from . import test_papilles_sauvages
 from . import test_relais_local
 from . import test_relais_vert
+from . import test_scop_epice
 from . import test_t_air_de_famille
 from . import test_vitafrais
