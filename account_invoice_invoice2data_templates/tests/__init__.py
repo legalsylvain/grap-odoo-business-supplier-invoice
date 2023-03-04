d336cf22a87e9f6bc1683c69dd20c6ed7b24d067
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-03-05 00:17:37 +0100

    [ADD] Teddy Bear

--- account_invoice_invoice2data_templates/tests/__init__.py
+++ account_invoice_invoice2data_templates/tests/__init__.py
@@ -2,6 +2,7 @@ from . import test
 
 
 from . import test_agrosourcing
+from . import test_brasserie_teddy_bear
 from . import test_comptoir_des_lys
 from . import test_coop_de_yenne
 from . import test_croc_jbg_sas
