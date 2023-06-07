375d5c11f094412d7f34b9d9fe8d8b76bac2a7a9
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-06-07 17:17:00 +0200

    [FIX] grap_account_invoice_invoice2data_templates : Teddy bear template. do not force to set space at beginning of the line. Allow to have negative amount.

--- grap_account_invoice_invoice2data_templates/tests/test_brasserie_teddy_bear.py
+++ grap_account_invoice_invoice2data_templates/tests/test_brasserie_teddy_bear.py
@@ -7,9 +7,9 @@ from .test_module import TestModule
 
 
 class TestBrasserieTeddyBear(TestModule):
-    def test_brasserie_teddy_bear(self):
+    def test_brasserie_teddy_bear_01(self):
         self._test_supplier_template(
-            "brasserie-teddy-beer__2222-12-01__FA2212-3445.pdf",
+            "brasserie-teddy-beer__2022-12-01__FA2212-3445.pdf",
             line_qty=4,
             expected_values={
                 "issuer": "Brasserie Teddy Bear",
@@ -27,6 +27,30 @@ class TestBrasserieTeddyBear(TestModule):
                     "quantity": 6,
                     "price_unit": 3.7533,
                     "price_subtotal": 22.52,
+                }
+            ],
+        )
+
+    def test_brasserie_teddy_bear_02(self):
+        self._test_supplier_template(
+            "brasserie-teddy-beer__2023-05-30__FA2305-3709.pdf",
+            line_qty=14,
+            expected_values={
+                "issuer": "Brasserie Teddy Bear",
+                "date": datetime(day=30, month=5, year=2023),
+                "date_due": datetime(day=30, month=6, year=2023),
+                "invoice_number": "FA2305-3709",
+                "amount_untaxed": 122.68,
+                "amount": 147.20,
+            },
+            expected_lines=[
+                {
+                    "product_code": "Stout33",
+                    "product_name": "Complètement Stout - 33cl",
+                    "vat_code": "20%",
+                    "quantity": 12,
+                    "price_unit": 1.68833,
+                    "price_subtotal": 20.26,
                 }
             ],
         )
